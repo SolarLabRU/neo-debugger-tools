08d0b879bdcccf7eec0c1c9e5da1264c97c0e7f1
Сергей Колеватых <s.kolevatykh@solarl.ru>
2018-09-20 15:07:42 +0300

    Added the ability to work in the test with multiple smart contracts

---
+++ NEO-Emulator/ScriptTable.cs
@@ -0,0 +1,35 @@
+﻿using Neo.Lux.Utils;
+using Neo.VM;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo.Emulation
+{
+    /// <summary>
+    /// Implementation of <see cref="IScriptTable"/> interface
+    /// </summary>
+    public class ScriptTable : IScriptTable
+    {
+        private Dictionary<string, byte[]> scripts = null;
+
+        public ScriptTable()
+        {
+            scripts = new Dictionary<string, byte[]>();
+        }
+
+        public void AddScript(string address, byte[] script)
+        {
+            AddScript(address.AddressToScriptHash(), script);
+        }
+
+        public void AddScript(byte[] script_hash, byte[] script)
+        {
+            scripts.Add(script_hash.Reverse().ToHexString(), script);
+        }
+
+        public byte[] GetScript(byte[] reverse_script_hash)
+        {
+            return scripts[reverse_script_hash.ToHexString()];
+        }
+    }
+}
